89d94da91ae9d3ddd9d4210d57c1d6085685ea36
saad shaikh <saad.shaikh0@gmail.com>
2020-02-04 21:18:44 +0530

    added django heroku

--- kjcodedecode/settings.py
+++ kjcodedecode/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ LOGIN_URL="/code/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 STATIC_ROOT="assets"
+
+django_heroku.settings(locals())
