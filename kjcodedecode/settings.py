584fb92d7688df63fd17b3531594147675221e20
saad shaikh <saad.shaikh0@gmail.com>
2020-02-04 20:40:15 +0530

    enviormen vaiarables added

--- kjcodedecode/settings.py
+++ kjcodedecode/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '65r#r7h!$u*43@m1bcai20^s#g9s)r(t#!8q-nrq_-1eu^3-*)'
-
+# SECRET_KEY = '65r#r7h!$u*43@m1bcai20^s#g9s)r(t#!8q-nrq_-1eu^3-*)'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kjcode.herokuapp.com",]
 
 
 # Application definition
